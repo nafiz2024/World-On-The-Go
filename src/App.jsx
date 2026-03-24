6b57227c23b108a76bb3aa9236465414da444793
Nafiz Alam <nafizalam.dev@gmail.com>
2026-03-25 04:10:37 +0600

    Complete Data Patching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
 
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
+
 function App() {
-  
+
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Data Loading...</p>}>
+      <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {use} from 'react';
+import Country from "../Country/Country.jsx";
 
-const countries = () => {
+const countries = ({countriesPromise}) => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
     return (
         <div>
-            <h1>In The Countries</h1>
+            <h1>In The Countries: {countries.length}</h1>
+            {
+                countries.map(country => <Country country={country}></Country>)
+            }
         </div>
     );
 };
